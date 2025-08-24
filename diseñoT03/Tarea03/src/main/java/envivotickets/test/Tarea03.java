29c052200176b2b911a7c94952fc0d618aae3b94
Jos-Ivan <joseivansalazarmacias@gmail.com>
2025-08-23 21:52:11 -0500

    Extract Class
    Extract Class

--- diseñoT03/Tarea03/src/main/java/envivotickets/test/Tarea03.java
+++ diseñoT03/Tarea03/src/main/java/envivotickets/test/Tarea03.java
@@ -14,12 +14,15 @@ public class Tarea03 {
         Evento evento = fabrica.crearEvento("teatro");
         System.out.println(evento.obtenerDetalles());
         System.out.println("Precio base: " + evento.obtenerPoliticaPrecios().obtenerPrecioBase());
+
+        // Creacion y verificacion de Eventos completada (Texto para reducir Clase en partes)
         NotificadorEventos notificador = new NotificadorEventos();
         Usuario u1 = new Usuario("Miguel");
         Administrador admin = new Administrador("Laura");
         notificador.agregarObservador(u1);
         notificador.agregarObservador(admin);
         notificador.notificarObservadores("Cambio de horario del evento.");
+        //Acciones de Notifier
         Asiento a1 = new Asiento("A1", 50, EstadoDisponibilidad.DISPONIBLE);
         Asiento a2 = new Asiento("A2", 50, EstadoDisponibilidad.AGOTADO);
         SeccionAsientos vip = new SeccionAsientos("VIP");
@@ -27,5 +30,6 @@ public class Tarea03 {
         vip.agregar(a2);
         System.out.println("Precio total sección VIP: " + vip.obtenerPrecio());
         System.out.println("Disponibilidad: " + vip.obtenerDisponibilidad());
+        // Verificacion del sistema
     }
 }
