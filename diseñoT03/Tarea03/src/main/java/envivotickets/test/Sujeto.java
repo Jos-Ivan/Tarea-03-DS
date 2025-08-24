16a8be9ca845e80b75f4cdeda5965acd7c7af732
Jos-Ivan <joseivansalazarmacias@gmail.com>
2025-08-23 22:01:38 -0500

    Remove Unnecessary Abstraction.

--- diseñoT03/Tarea03/src/main/java/envivotickets/test/Sujeto.java
+++ diseñoT03/Tarea03/src/main/java/envivotickets/test/Sujeto.java
@@ -1,6 +1,7 @@
 package envivotickets.test;
 public interface Sujeto {
     void agregarObservador(Observador o);
+    // Se cambia la abstraccion, no es necesario en este caso por soporte de notificaciones
     void removerObservador(Observador o);
     void notificarObservadores(String mensaje);
-}
+}   
