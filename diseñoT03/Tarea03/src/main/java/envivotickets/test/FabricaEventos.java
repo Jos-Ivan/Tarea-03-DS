65c79cf9ff21d47cf2f737492fbb88249f7e7fb0
Jos-Ivan <joseivansalazarmacias@gmail.com>
2025-08-23 22:04:23 -0500

    Remove Middle Man

--- diseñoT03/Tarea03/src/main/java/envivotickets/test/FabricaEventos.java
+++ diseñoT03/Tarea03/src/main/java/envivotickets/test/FabricaEventos.java
@@ -1,5 +1,5 @@
 package envivotickets.test;
 public abstract class FabricaEventos {
     public abstract Evento crearEvento(String tipo);
-    
+    //Evento e = new FabricaEventosConcreta().crear("CONCIERTO"); Colocar en Clases Anexas si se quiere remove middle man
 }
